a65f76942c68148a85d742b7868860e02b86c841
puneet75way <puneet@75way.com>
2025-01-29 12:54:16 +0530

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+
+
+const Header = () => {
+  return (
+   <>
+   <div>
+
+    <div>
+        <div>
+
+        <img src="../../assets/Frame 3.svg" alt="" />
+        <h1>JobHuntly</h1>
+        </div>
+        <div>
+            <h3>Find Jobs</h3>
+            <h3>
+                Browse Companies
+            </h3>
+        </div>
+    </div>
+
+    <div>
+
+    </div>
+   </div>
+   </>
+  )
+}
+
+export default Header
